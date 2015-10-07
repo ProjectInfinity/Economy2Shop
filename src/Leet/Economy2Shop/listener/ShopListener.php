48dab0da34dbc0a4e94d8fdc04ba15b58804b292
ProjectInfinity <a.sjur91@gmail.com>
2015-10-07 02:12:29 +0200

    Fix out of bounds error.

--- src/Leet/Economy2Shop/listener/ShopListener.php
+++ src/Leet/Economy2Shop/listener/ShopListener.php
@@ -266,7 +266,7 @@ class ShopListener implements Listener {
             $item = Item::get(intval($item), 0, $quantity);
         } elseif(count(explode(':', $item)) > 1) {
             $itemData = explode(':', $item);
-            $item = Item::get(intval($itemData[0]), intval($itemData[2]), $quantity);
+            $item = Item::get(intval($itemData[0]), intval($itemData[1]), $quantity);
         } else {
             $event->getPlayer()->sendMessage(TextFormat::RED.'Invalid item.');
             return;
